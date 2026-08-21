88de87d7a5d7da770e1f619d7e58167b73cb4cd5
Xomagat <zafarovsamir576@gmail.com>
2026-08-21 14:07:24 +0300

    Add start arguments

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
+#include <cstring>
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cstring>
 #include <fstream>
 #include <sstream>
 
@@ -9,28 +11,58 @@
 
 #include "libs/Environment.h"
 
-int main()
+void info()
 {
-    std::ifstream file("D:/Projects/JustProgrammigProjects/C++ project/FABSharp/test.fab");
-    std::stringstream buffer;
+    std::cout << "FAB# Interpreter\tv0.3" << std::endl
+              << std::endl
+              << "Start arguments:" << std::endl
+              << "--help/-h - show this messege" << std::endl
+              << std::endl
+              << "How run the script?" << std::endl
+              << "interpreter_path script_path.fab" << std::endl;
+}
 
-    if (!file.is_open())
+int main(int argc, char** argv)
+{
+    if (argc > 1)
     {
-        std::cout << "Error opening file" << std::endl;
+        if (std::strcmp(argv[1], "--help") == 0 || std::strcmp(argv[1], "-h") == 0)
+        {
+            info();
+            return 0;
+        }
+
+        std::ifstream file(argv[1]);
+        std::stringstream buffer;
+
+        if (!file.is_open())
+        {
+            std::cout << "Error opening file!" << std::endl;
+
+            return 1;
+        }
+
+        buffer << file.rdbuf();
+
+        std::string input = buffer.str();
+
+        auto tokens = Lexer(input).tokenize();
+
+        auto expression = Parser(tokens).parse();
+
+        Environment global;
+
+        for (auto& expr : expression)
+        {
+            expr->execute(global);
+        }
+
+        return 0;
     }
-
-    buffer << file.rdbuf();
-
-    std::string input = buffer.str();
-
-    auto tokens = Lexer(input).tokenize();
-
-    auto expression = Parser(tokens).parse();
-
-    Environment global;
-
-    for (auto& expr : expression)
+    else
     {
-        expr->execute(global);
+        std::cout << "Invalid argument!" << std::endl;
+
+        return 2;
     }
 }
